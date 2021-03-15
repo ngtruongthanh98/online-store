d2ee9408500c9f8f2d32f0cf4fd06e788b622bf0
thanhnguyen98 <ngtruongthanh98@gmail.com>
2021-03-15 17:50:20 +0700

    try to create button function onAddToCart

--- src/App.js
+++ src/App.js
@@ -3,61 +3,73 @@ import Product from "./components/Product";
 class App extends PureComponent {
   constructor(props) {
     super(props);
-    this.btnRef= React.createRef();
-  }
-  
-  onClick() {
-    console.log("Here is app component");
+    this.state = {
+      products: [
+        {
+          id: 1,
+          name: "Iphone 12 Pro Max 256GB",
+          price: "999 USD",
+          altImg: "Iphone 12 Pro Max 256GB",
+          srcImg:
+            "https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-xanh-duong-new-600x600-200x200.jpg",
+          status: true,
+        },
+        {
+          id: 2,
+          name: "Iphone 12 Pro Max 128GB",
+          price: "999 USD",
+          altImg: "Iphone 12 Pro Max 128GB",
+          srcImg:
+            "https://24hstore.vn/images/products/2020/10/14/large/iphone-12-pro-max-blue.jpg",
+          status: true,
+        },
+        {
+          id: 3,
+          name: "AirPods Pro",
+          price: "299 USD",
+          altImg: "AirPods Pro",
+          srcImg:
+            "https://bizweb.dktcdn.net/100/324/157/products/3-94574bf8-44a3-40d8-87e2-2981053c4660.jpg?v=1602840587000",
+          status: true,
+        },
+      ],
+      isActive: true,
+    };
   }
 
-  onAddProduct = () => {
-    console.log(this.btnRef.current.value);
-  }
+  //////////////////////////////////////////////
+  onAddToCart = (id) => {
+    alert(this.state.products[id].name);
+    // console.log(this.state.products.name)
+    console.log(id)
+  };
 
   render() {
-    var products = [
-      {
-        id: 1,
-        name: "Iphone 12 Pro Max 256GB",
-        price: "999 USD",
-        altImg: "Iphone 12 Pro Max 256GB",
-        srcImg:
-          "https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-xanh-duong-new-600x600-200x200.jpg",
-        status: true,
-      },
-      {
-        id: 2,
-        name: "Iphone 12 Pro Max 128GB",
-        price: "999 USD",
-        altImg: "Iphone 12 Pro Max 128GB",
-        srcImg:
-          "https://24hstore.vn/images/products/2020/10/14/large/iphone-12-pro-max-blue.jpg",
-        status: true,
-      },
-      {
-        id: 3,
-        name: "AirPods Pro",
-        price: "299 USD",
-        altImg: "AirPods Pro",
-        srcImg:
-          "https://bizweb.dktcdn.net/100/324/157/products/3-94574bf8-44a3-40d8-87e2-2981053c4660.jpg?v=1602840587000",
-        status: true,
-      },
-    ];
-
-    let elements = products.map((product, index) => {
+    let elements = this.state.products.map((product, index) => {
+      let result = "";
       if (product.status) {
-        return (
-          <Product
-            key={product.id}
-            price={product.price}
-            altImg={product.altImg}
-            srcImg={product.srcImg}
-          >
-            {product.name}
-          </Product>
+        result = (
+          <tr key={product.id}>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+            <td>
+              <Product                
+                altImg={product.altImg}
+                srcImg={product.srcImg}
+              />
+            </td>
+            <td>
+              <span className="label label-success">{product.price}</span>
+            </td>
+            <td>
+              <a className="btn btn-primary" onClick={this.onAddToCart(index)}>
+                Buy Now
+              </a>
+            </td>
+          </tr>
         );
       }
+      return result;
     });
 
     return (
@@ -70,35 +82,19 @@ class App extends PureComponent {
 
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <div className="panel panel-danger">
-                <div className="panel-heading">
-                  <h3 className="panel-title">Add product</h3>
-                </div>
-                <div className="panel-body">
-                  <div className="form-group">
-                    <label>Product Name</label>
-                    <input type="text" className="form-control" ref={this.btnRef}/>
-                  </div>
-                  <button type="submit" className="btn btn-primary" onClick={this.onAddProduct}>
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              {elements}
-            </div>
-
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <button
-                type="button"
-                className="btn btn-warning"
-                onClick={this.onClick}
-              >
-                Click Me!
-              </button>
+            <div className="row">
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>{elements}</tbody>
+              </table>
             </div>
           </div>
         </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -10,25 +10,12 @@ class Product extends PureComponent {
   //   alert(this.props.children + '___' + this.props.price);
   // }
 
-  onAddToCart = () => {
-    alert(this.props.children + "___" + this.props.price);
-  };
-
   render() {
     return (
       <div>
         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
           <div className="thumbnail">
             <img src={this.props.srcImg} alt={this.props.altImg} />
-            <div className="caption">
-              <h3>{this.props.children}</h3>
-              <p>{this.props.price}</p>
-              <p>
-                <a className="btn btn-primary" onClick={this.onAddToCart}>
-                  Buy Now
-                </a>
-              </p>
-            </div>
           </div>
         </div>
       </div>
